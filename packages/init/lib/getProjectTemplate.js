23cd0547832ba53c10f3149357d3a5c4ea768c72
snnaenu <455342265@qq.com>
2022-11-16 16:36:54 +0800

    cli

--- packages/init/lib/getProjectTemplate.js
+++ packages/init/lib/getProjectTemplate.js
@@ -1,7 +1,36 @@
 const { request } = require('@freelog-cli/utils');
 
 module.exports = function() {
-  return request({
-    url: '/project/template',
-  });
+  return new Promise((resolve)=>{
+     resolve([
+      {
+          "name": "freelog主题-vue2项目模板", 
+          "npmName": "@freelog-cli/freelog-theme-vue2-template", 
+          "version": "1.0.2", 
+          "type": "normal", 
+          "installCommand": "npm install", 
+          "startCommand": "npm run serve", 
+          "ignore": [
+              "**/public/**"
+          ], 
+          "tag": [
+              "project"
+          ], 
+          "buildPath": "dist"
+      }, 
+      {
+          "name": "freelog主题-vue3项目模板", 
+          "npmName": "@freelog-cli/freelog-theme-vue3-template", 
+          "version": "1.0.2", 
+          "type": "normal", 
+          "startCommand": "npm run serve", 
+          "ignore": [
+              "**/public/**"
+          ], 
+          "tag": [
+              "project"
+          ], 
+          "buildPath": "dist"
+      }])
+  })
 };
